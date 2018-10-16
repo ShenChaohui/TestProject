8613fce23f37d2e216f4903f5ff73c0659c24910
Admin <1205155699@qq.com>
2018-10-16 11:07:54 +0800

    定位结果展示

--- app/src/main/java/com/genius/zydl/testproject/activitys/LocationActivity.java
+++ app/src/main/java/com/genius/zydl/testproject/activitys/LocationActivity.java
@@ -3,22 +3,29 @@ package com.genius.zydl.testproject.activitys;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.geocoder.GeocodeResult;
+import com.amap.api.services.geocoder.GeocodeSearch;
+import com.amap.api.services.geocoder.RegeocodeQuery;
+import com.amap.api.services.geocoder.RegeocodeResult;
 import com.genius.zydl.testproject.R;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
 
 public class LocationActivity extends BasicActivity {
     private MapView mMapView;
+    private TextView mLat, mLon, mAddress;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mMapView.onCreate(savedInstanceState);
-        AMap aMap =mMapView.getMap();
+        AMap aMap = mMapView.getMap();
         MyLocationStyle locationStyle = new MyLocationStyle();
         locationStyle.interval(2000);
         locationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);
@@ -31,6 +38,22 @@ public class LocationActivity extends BasicActivity {
         aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location location) {
+                mLat.setText("纬度：" + location.getLatitude());
+                mLon.setText("经度：" + location.getLongitude());
+                GeocodeSearch geocodeSearch = new GeocodeSearch(context);
+                geocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+                    @Override
+                    public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+                        mAddress.setText("位置：" + regeocodeResult.getRegeocodeAddress().getFormatAddress());
+                    }
+
+                    @Override
+                    public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
+
+                    }
+                });
+                RegeocodeQuery query = new RegeocodeQuery(new LatLonPoint(location.getLatitude(), location.getLongitude()), 200, GeocodeSearch.AMAP);
+                geocodeSearch.getFromLocationAsyn(query);
 
             }
         });
@@ -45,6 +68,9 @@ public class LocationActivity extends BasicActivity {
     @Override
     protected void initView() {
         mMapView = findViewById(R.id.mapview);
+        mLat = findViewById(R.id.tv_location_lat);
+        mLon = findViewById(R.id.tv_location_lon);
+        mAddress = findViewById(R.id.tv_location_address);
     }
 
     @Override
@@ -61,11 +87,13 @@ public class LocationActivity extends BasicActivity {
         super.onDestroy();
         mMapView.onDestroy();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
